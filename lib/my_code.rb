f51196ff7bf285d7eb8b4600068325fcc1d79034
Chuck Smith <chuck@eclecticsaddlebag.com>
2019-08-23 15:58:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,13 +1,13 @@
 # Your Code Here
 
-def my_own_map(set)
+def map(set)
   new = []
-  i = 0 
-  while i < set.length 
+  i = 0
+  while i < set.length
     new.push(yield(set[i]))
     i += 1
   end
-  new 
+  new
 end
 
 
